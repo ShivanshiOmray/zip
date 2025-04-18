f4f57d0e0725912a676df71b02dc5cb1838f4d58
Shivanshi Omray <161451624+ShivanshiOmray@users.noreply.github.com>
2025-04-18 16:18:20 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,20 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import Navigation from './src/navigation/Navigation';
+import { Provider } from 'react-redux';
+import { persistor, store } from './src/redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+
+const App = () => {
+  return (
+   <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <Navigation/>
+    </PersistGate>
+   </Provider>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
